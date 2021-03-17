bf6caf8682507150055398e789fa54ac2db22eaf
Igor Kaizer <igorkaizer1@gmail.com>
2021-03-16 21:03:19 -0300

    Projeto criado para aprender programacao

---
+++ Hello Word/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Hello_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hora do Intervalo");
+
+            int funk = 2000;
+            int proibidao = 18;
+
+            int putaria = funk + proibidao;
+
+           
+
+            //variavel do tipo inteiro
+
+
+            string Nome = "Igor";
+            string Sobrenome = "Kaizer";
+
+            string NomeCompleto = Nome +" "+Sobrenome;
+
+
+            Console.WriteLine(NomeCompleto);
+
+        }
+        
+    }
+}
